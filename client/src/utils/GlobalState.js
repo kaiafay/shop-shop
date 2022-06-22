a0c57758c412f5a4d5788a337f0d1a6abfd9309d
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-21 20:45:25 -0700

    use context object for global state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -4,3 +4,23 @@ import { useProductReducer } from './reducers';
 // instantiate the global state object
 const StoreContext = createContext();
 const { Provider } = StoreContext;
+
+// instantiate initial global state
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: ''
+    });
+    // confirm it works
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+// custom react hook
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
+
