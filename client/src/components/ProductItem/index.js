67ba2489e1c70a7ca75899b6349862f8c51fbe82
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-22 18:34:47 -0700

    implement adding items to cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { pluralize } from "../../utils/helpers"
 
 function ProductItem(item) {
+  const [state, dispatch] = useStoreContext();
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...item, purchaseQuantity: 1 }
+    });
+  };
+
   const {
     image,
     name,
@@ -24,7 +35,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button>Add to cart</button>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
